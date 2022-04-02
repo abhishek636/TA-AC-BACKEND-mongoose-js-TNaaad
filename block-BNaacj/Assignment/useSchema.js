f91d5c7a9c4f020958762773e9edaa771dc3f791
Abhishek Bhatt <bhattabhishek636@gmail.com>
2022-04-02 23:00:11 +0530

    block-BNaacj

--- block-BNaacj/Assignment/useSchema.js
+++ block-BNaacj/Assignment/useSchema.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    password: {type: String, minlength: 5 , maxlength: 15}
+    password: {type: String, minlength: 5 , maxlength: 15},
     createdAt: {type:Date, default: new Date()}
 })
